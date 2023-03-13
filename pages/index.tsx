2dde8b7f9f91369abec0d8cf52a37bbbdda0413d
--local <--local>
2023-03-13 06:27:42 -0300

    styles for footter created

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import style from "./Home.module.css";
 import Experience from "../components/Experience";
 import Services from "../components/Services";
 
+import { BsWhatsapp, BsTwitter, BsLinkedin } from "react-icons/bs";
+// import {CiTwitter} from "react-icons/ci"
 // fonts
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,16 +24,33 @@ export default function Home({ skills }: params) {
       </Head>
       <main style={inter.style}>
         <Header skills={skills} />
-        <Services/>
+        <Services />
         <Experience />
         <section className={style.footer}>
-          <h2>CONTACT</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-            soluta quisquam magni a nisi totam dolores error laboriosam ipsum
-            quae expedita consequatur illum, cum, quas sit officia temporibus!
-            Fugiat, iste.
-          </p>
+          <div className={style.content}>
+            <h2>Contacto</h2>
+            <p>
+              Internet y la teconología pueden ser abrumadoras. En caso de
+              necesitar asesoría para, o el desarrollo completo, de un proyecto
+              que tengas en mente, podemos coordinar una primer llamada sin
+              costo. ¡Mucha suerte!
+            </p>
+            <div className={style.networks}>
+              <a className={style.social}>
+                <BsLinkedin />
+              </a>
+              <a className={style.social}>
+                <BsTwitter color="white" />
+              </a>
+              <a className={style.social}>
+                <BsWhatsapp />
+              </a>
+              <button className={style.directContact}>+59 11-3213-4807</button>
+              <button className={style.directContact}>
+                nahuelrabeywork@gmail.com
+              </button>
+            </div>
+          </div>
         </section>
       </main>
     </>
